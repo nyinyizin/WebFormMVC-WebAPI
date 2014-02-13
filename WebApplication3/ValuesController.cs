4495c56ced4ebcf1561b5bad72c7185ae421abbb
ubuntu <nyinyizin11@gmail.com>
2014-02-12 20:32:29 -0800

    Web Form in MVC Pattern by using Web API

---
+++ WebApplication3/ValuesController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication3
+{
+    public class ValuesController:ApiController
+    {
+        [HttpGet]
+        public string GetString()
+        {
+            return "i am from api";
+        }
+    }
+}
